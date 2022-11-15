47f3f58b56de6d5798764991de06f037cc7f4160
Breiz-sk <53227343+Breiz-sk@users.noreply.github.com>
2022-11-15 09:46:55 +0100

    Add files via upload

---
+++ AppMajeur/AppMajeur/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMajeur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age ;
+            string ageStr;
+
+            Console.WriteLine("Votre age : ");
+            ageStr = Console.ReadLine();
+            age = byte.Parse(ageStr);
+            Console.WriteLine(age);
+            
+            if (age >= 18)
+            {    
+                Console.WriteLine("Majeur");
+                if (age > 65)
+                {
+                    Console.WriteLine("Senior");
+                }
+            }
+            
+            else
+            {
+                Console.WriteLine("Mineur");
+            }
+            ageStr = Console.ReadLine();
+        }
+    }
+}
---
+++ AppNegatifsPositifs/AppNegatifsPositifs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNegatifsPositifs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre = 0, cptPositif=0, cptNegatif=0, Somme = 0;
+            double moyenne = 0;
+            string nombreStr;
+
+            do
+            {
+                Console.Write("Saisir 0 pour terminer: ");
+                //nombreStr = Console.ReadLine();
+                //nombre = int.Parse(nombreStr);
+                nombre = int.Parse(Console.ReadLine());
+                Console.WriteLine(nombre);
+                if (nombre > 0)
+                {
+                    cptPositif++;
+                    moyenne += nombre;
+
+                }
+                else
+                {
+                    cptNegatif++;
+                    Somme += nombre;
+
+                }
+            } while (nombre != 0);
+            moyenne /= cptPositif;
+            //moyenne2 /= cptNegatif;
+            Console.WriteLine("Moyenne nombre positifs : "+moyenne);
+            //Console.WriteLine("Moyenne nombre négatifs : " + moyenne2);
+            Console.WriteLine("Somme nombre négatifs : " + Somme);
+            Console.WriteLine("fin du programme");
+            Console.ReadKey();
+        }
+    }
+}
