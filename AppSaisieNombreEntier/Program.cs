647093328930c6bbf4c7c28ad60af5050000a8de
Breiz-sk <darkwoodse@gmail.com>
2022-11-21 21:19:59 +0100

    New exo

---
+++ AppSaisieNombreEntier/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSaisieNombreEntier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valeur;
+            string strValeur;
+            
+
+            Console.Write("SAISIR UN ENTIER : ") ;
+            strValeur = Console.ReadLine();
+            
+            while (int.TryParse(strValeur, out valeur)==false ) 
+            {
+                Console.WriteLine("La valeur saisie n'est pas un ENTIER") ;
+                Console.Write("SAISIR UN ENTIER : ") ;
+                strValeur = Console.ReadLine();
+            }
+            Console.WriteLine("La valeur saisie est bien un ENTIER") ;
+            
+            
+
+        }
+    }
+}
